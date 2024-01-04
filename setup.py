e4f0f1246d0cc76ff5bf82ae058602eba3a8e574
alex <alexmanson_lu@outlook.com>
2024-01-04 20:07:11 +0800

    new pt file and new tracking_node_v1.py now working

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
-
+import os
 package_name = 'object_detection_avix'
+# Define the path to your engine file
+engine_file = os.path.join(package_name, 'yolov8n.engine')
+
 
 setup(
     name=package_name,
@@ -10,6 +13,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), [engine_file]),
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +25,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'tracking_node = object_detection_avix.tracking_node_v1:main',
         ],
     },
 )
