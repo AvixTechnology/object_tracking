5d3e51a1d49bf4fb798180e58cb19f34f14e0874
alexmanson <alexmanson_lu@outlook.com>
2024-02-28 15:39:12 +0800

    v5 for action server to change status of the system based on new system of mq3_bringup

--- object_detection_avix/tracking_node_v5.py
+++ object_detection_avix/tracking_node_v5.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
+from avix_action.action import ObjectDetectionCMD
 from std_msgs.msg import Bool, Int32MultiArray, Float32MultiArray, Int32, ByteMultiArray
 from ultralytics import YOLO
 from avix_msg.msg import TrackingUpdate, ObjectDetection, ObjectDetections, TrackingCommand
@@ -10,6 +11,11 @@ import struct
 from object_detection_avix.object_detection_util import KalmanFilter ,ReIDTrack
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np  
+from rclpy.action import ActionServer
+
+# tracking node v5
+# 1. will have icp control state (to do different thing)
+
 
 
 # for reading engin
@@ -30,8 +36,13 @@ class TrackingNode(Node):
         # Subscribe to necessary topics
         self.target_subscriber = self.create_subscription(Int32, '/object_detection/tracking_target_id', self.target_id_callback, 10)
         self.image_subscriber = self.create_subscription(Image, '/ktg_gimbal/image_raw', self.image_callback, 10)
-        self.enable_subscriber = self.create_subscription(TrackingCommand, '/icp_interface/tracking_cmd', self.enable_callback, 10)
-        
+        #self.enable_subscriber = self.create_subscription(TrackingCommand, '/icp_interface/tracking_cmd', self.status_callback, 10)
+        self.cmd_action_server =  ActionServer(
+            self,
+            ObjectDetectionCMD,
+            'object_detection/cmd',
+            self.command_callback)
+
         # Publisher for the deviation 
         self.deviation_publisher = self.create_publisher(TrackingUpdate, '/object_detection/target_deviation', 10)
         self.box_publisher = self.create_publisher(ObjectDetections, '/object_detection/detections', 10) # bytes array is not working yet
@@ -40,7 +51,6 @@ class TrackingNode(Node):
         # initialize the parameter
         # self.declare_parameter('tracking_enabled', False)
         #self.tracking_enabled = self.get_parameter('tracking_enabled').value
-        self.tracking_enabled = True
 
         self.declare_parameter('confidence', 0.6)
         self.confidence = self.get_parameter('confidence').value
@@ -79,12 +89,15 @@ class TrackingNode(Node):
         self.KF_initailized = False
 
         self.initializing = False
+
+        # state of this system
+        self.state_tracking = False
+        self.state_following = False
+
         # node created
         self.detection = ObjectDetection()
         self.objects_data = ObjectDetections()
         self.get_logger().info(f'Object Detection Node created')
-
-
 
 
     def image_callback(self, msg):
@@ -93,7 +106,7 @@ class TrackingNode(Node):
             return
 
         # not run the model if tracking is disabled
-        if not self.tracking_enabled:
+        if not self.state_tracking:
             return
         
         # Convert ROS Image message to CV2 format and pass it to model
@@ -127,7 +140,7 @@ class TrackingNode(Node):
             tcls = t.cls
             c,  id = int(tcls), int(tid)
             x1,y1,x2,y2=tlbr[0],tlbr[1],tlbr[2],tlbr[3]
-            if(id  == self.target_id):
+            if(id  == self.target_id and self.state_following):
                 self.follow((x2+x1)/2,(y2+y1)/2,x2-x1,y2-y1)
             self.detection.id = id  # Assign the detection ID
             self.detection.bbox = tlbr  # Replace with actual bbox coordinates
@@ -175,19 +188,34 @@ class TrackingNode(Node):
         self.KF_y = KalmanFilter(F = F, H = self.H, Q = Q, R = R, x0=y_0)
         self.KF_initailized = True
 
-    def enable_callback(self, msg):
+    def command_callback(self, goal_handle):
+        self.get_logger().info('Changing Status Based on command')
+        result = ObjectDetectionCMD.Result()
+        tracking_enabled = goal_handle.request.tracking_enabled
+        folloing_enabled = goal_handle.request.following_enabled
+        following_id = goal_handle.request.tracking_id
+
         if self.initializing:
             self.get_logger().warn(f'Object Detection Model still initializing, please send command later')
-            return
-        if msg.tracking_enabled and msg.tracking_id > -1:
-            # start tracking
-            self.tracking_enabled = True
-            self.target_id = msg.tracking_id
-        else:
-            # disable tracking
-            self.target_id = -1
-            self.KF_initailized = False
-            self.tracking_enabled = False
+            goal_handle.abort()
+            result.error_code = 1
+            return result
+        
+        if tracking_enabled:
+            self.state_tracking = True
+
+            if(folloing_enabled):
+                self.state_following = True
+                self.target_id = following_id
+            else:
+                self.state_following = False
+                self.target_id = -1
+
+        goal_handle.succeed()
+        result.error_code = 0
+
+        return result
+        
 
     def target_id_callback(self, msg):
         # Assuming msg.data contains the ID of the target to track
